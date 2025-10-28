16d62ce093327762d24035c02f13fb37a5cd8d0b
ukk0 <sami.ukk91@gmail.com>
2025-10-28 22:00:25 +0100

    reorg steps

--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -7,21 +7,25 @@ def step_open_inventor_page(context):
     context.page.context.add_cookies([login_cookie()])
     context.page_object.navigate_to_inventory_page()
 
-@then("I should see all products with a title, picture, description and a price")
-def step_verify_product_contents(context):
-    context.page_object.verify_inventory_item_contents()
-
 @when("I add two items to the cart")
 def step_add_two_items_to_cart(context):
     context.page_object.add_items_to_cart(amount_of_items=2)
 
-@then("I should see two items indicated by the shopping cart badge")
-def step_cart_should_have_two_items(context):
-    assert context.page_object.cart_item_count() == 2
-
 @when("I remove one of the items from cart")
 def step_remove_item_from_cart(context):
     context.page_object.remove_items_from_cart(amount_of_items=1)
+
+@when("I open the shopping cart")
+def step_open_shopping_cart(context):
+    context.page_object.open_shopping_cart()
+
+@then("I should see all products with a title, picture, description and a price")
+def step_verify_product_contents(context):
+    context.page_object.verify_inventory_item_contents()
+
+@then("I should see two items indicated by the shopping cart badge")
+def step_cart_should_have_two_items(context):
+    assert context.page_object.cart_item_count() == 2
 
 @then("I should see the amount of cart items reduced to one")
 def step_cart_items_are_reduced(context):
--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -9,13 +9,25 @@ def step_open_login_page(context):
 def step_enter_valid_credentials(context):
     context.page_object.login_user_success()
 
-@then("I should be redirected without any errors")
-def step_redirected_without_error(context):
-    context.page_object.redirected_to_inventory_without_error()
-
 @when("I use a username for locked account and click 'Login'")
 def step_enter_locked_username(context):
     context.page_object.login_error_locked_user()
+
+@when("I leave the username field empty and click 'Login'")
+def step_missing_username(context):
+    context.page_object.login_missing_username()
+
+@when("I leave the password field empty and click 'Login'")
+def step_missing_password(context):
+    context.page_object.login_missing_password()
+
+@when("I enter a valid username and an incorrect password and click 'Login'")
+def step_invalid_credentials(context):
+    context.page_object.login_error_wrong_password()
+
+@then("I should be redirected without any errors")
+def step_redirected_without_error(context):
+    context.page_object.redirected_to_inventory_without_error()
 
 @then("I should see an error message about locked user")
 def step_locked_user_error(context):
@@ -23,29 +35,17 @@ def step_locked_user_error(context):
         "Epic sadface: Sorry, this user has been locked out."
     )
 
-@when("I leave the username field empty and click 'Login'")
-def step_missing_username(context):
-    context.page_object.login_missing_username()
-
 @then("I should see an error message about missing username")
 def step_missing_username_error(context):
     context.page_object.expect_login_error_with_text(
         "Epic sadface: Username is required"
     )
 
-@when("I leave the password field empty and click 'Login'")
-def step_missing_password(context):
-    context.page_object.login_missing_password()
-
 @then("I should see an error message about missing password")
 def step_missing_password_error(context):
     context.page_object.expect_login_error_with_text(
         "Epic sadface: Password is required"
     )
-
-@when("I enter a valid username and an incorrect password and click 'Login'")
-def step_invalid_credentials(context):
-    context.page_object.login_error_wrong_password()
 
 @then("I should see an error message about invalid credentials")
 def step_invalid_credentials_error(context):
