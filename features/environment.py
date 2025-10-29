3511b11df93ba3696e9ea5816900b6181fe902b7
ukk0 <sami.ukk91@gmail.com>
2025-10-29 22:40:54 +0100

    clean up class init

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,5 @@
 from utils.browser_factory import BrowserFactory
-from pages.login_page import LoginPage
-from pages.inventory_page import InventoryPage
-from pages.navigation_page import NavigationPage
-from pages.shopping_cart_page import ShoppingCartPage
-# from pages.checkout_page import CheckoutPage
+from utils.page_mapping import PAGE_OBJECTS
 from utils.config import HEADLESS
 
 
@@ -13,15 +9,10 @@ def before_all(context):
     context.browser_factory = factory
 
 def before_feature(context, feature):
-    if feature.name == "Login functionality":
-        context.page_object = LoginPage(context.page)
-    elif feature.name == "Inventory functionality":
-        context.page_object = InventoryPage(context.page)
-    elif feature.name == "Navigation functionality":
-        context.page_object = NavigationPage(context.page)
-    elif feature.name == "Shopping cart functionality":
-        context.page_object = ShoppingCartPage(context.page)
-    # TODO: Extend as needed.
+    page_class = PAGE_OBJECTS.get(feature.name)
+    if not page_class:
+        raise ValueError(f"No page object mapped for feature: {feature.name}")
+    context.page_object = page_class(context.page)
 
 def after_all(context):
     context.browser_factory.stop_execution()
---
+++ utils/page_mapping.py
@@ -0,0 +1,12 @@
+from pages.inventory_page import InventoryPage
+from pages.login_page import LoginPage
+from pages.navigation_page import NavigationPage
+from pages.shopping_cart_page import ShoppingCartPage
+
+
+PAGE_OBJECTS = {
+    "Login functionality": LoginPage,
+    "Inventory functionality": InventoryPage,
+    "Navigation functionality": NavigationPage,
+    "Shopping cart functionality": ShoppingCartPage,
+}
